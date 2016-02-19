162462b16bd58cbcc3547627b264a2d2eac896b9
Jake Shasteen <jakeshasteen@gmail.com>
2016-02-18 16:39:48 -0800

    Add basic server setup

---
+++ server/config/routes.js
@@ -0,0 +1,15 @@
+// require controllers here
+
+module.exports = function ( app, express ) {
+  /* GENRES */
+
+  /* MOVIES */
+
+  /* PREFS */
+
+  /* SESSIONS */
+
+  /* USERS */
+
+  /* VOTES */
+};
