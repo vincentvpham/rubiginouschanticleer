06bf4b869c61b64b288cb254b45c008f324682ae
Jing Lu <jinglu@Jings-MacBook.local>
2016-02-18 16:11:51 -0800

    implement genres table

---
+++ server/genres/genres.js
@@ -0,0 +1,10 @@
+var db = require( '../config/db' );
+var Sequelize = require( 'sequelize' );
+
+var Genre = db.define( 'genres',  {
+	genreName = Sequelize.STRING;
+} );
+
+Genre.sync();
+
+module.exports = Genre;
