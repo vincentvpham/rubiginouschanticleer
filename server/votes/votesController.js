eba69cdc58f781f7aa3b5a615b984e1f8e245aed
Jing Lu <jinglu@Jings-MacBook.local>
2016-02-19 15:49:22 -0800

    implement votes table & added comment for votes table controller

--- server/votes/votesController.js
+++ server/votes/votesController.js
@@ -7,6 +7,9 @@ module.exports = {
   addVote: function( req, res, next ) {
     console.log( 'TESTING: addVote', req.body );
     res.send( 'TESTING: vote added' );
+    // add vote to database
+    // check if there is a match in current session
+      // if so, send socket event to inform users of match
   }
   
 };
