6cf014983d6052958a5f71e4177a1ef428a81cd3
Jing Lu <jinglu@Jings-MacBook.local>
2016-02-19 12:25:23 -0800

    implement sessions_users table

---
+++ server/sessions_users/sessions_users.js
@@ -0,0 +1,16 @@
+var db = require( '../config/db' );
+var Sequelize = require( 'sequelize' );
+var User = require( '../users/users' );
+var Session = require( '../sessions/sessions' );
+
+var Session_User = db.define( 'sessions_users', {
+	user_id : Sequelize.INTEGER,
+	session_id : Sequelize.INTEGER
+} );
+
+Session_User.sync();
+
+Session_User.belongsTo( User, {foreignKey: 'user_id'} );
+Session_User.belongsTo( Session, {foreignKey: 'session_id'} );
+
+module.exports = Session_User; 
