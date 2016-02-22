8194365c5e0ab291e35beb70f2ba5bd838a07e13
Jing Lu <jinglu@Jings-MacBook.local>
2016-02-22 12:06:37 -0800

    will come back to it

--- server/users/usersController.js
+++ server/users/usersController.js
@@ -4,9 +4,45 @@ var jwt = require( 'jwt-simple' );
 module.exports = {
   getAllUsers: function() {},
 
-  signin: function() {},
+  signin: function (request, response) {
+  var username = request.body.username;
+  var password = request.body.password;
 
-  signup: function() {},
+  User.findOne({where: {username: username}})
+    .then(function (user) {
+      if (!user) {
+        response.redirect('/signin');
+      } else {
+          if (password === user.dataValues.password) {
+            var token = jwt.encode(user, 'secret');
+            response.json({token: token});
+          } else {
+            response.redirect('/signin');
+          }
+        } 
+    })
+  },
+
+  signup: function(request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+
+    User.findOne({where: {username: username}})
+      .then(function (user) {
+        if (user) {
+          response.send("username already exist, choose another username!");
+        } else {
+          User.create({
+            username: username,
+            password: password
+          })
+          .then(function (user) {
+            var token = jwt.encode(user, 'secret');
+            response.json({token: token});
+          });
+        }
+      })
+  },
 
   signout: function() {}
 
