e5daabb670e81eb2f400a945c86dc9f6ddb5fd00
Jing Lu <jinglu@Jings-MacBook.local>
2016-02-24 19:51:07 -0800

    small fixes

--- server/sessions_users/sessions_usersController.js
+++ server/sessions_users/sessions_usersController.js
@@ -41,6 +41,7 @@ module.exports = {
     // responds with data for that user in that session
     var session = parseInt( req.params.session_id );
     var user = parseInt( req.params.user_id );
+    console.log("getSessionUserBySessionAndUser", session, user);
     if( !session ) {
       res.status( 400 );
       res.send( "Invalid session id" );
