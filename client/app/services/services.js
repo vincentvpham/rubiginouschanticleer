c020034127211b61cb6f413448753f10758ba190
Jake Shasteen <jakeshasteen@gmail.com>
2016-02-24 18:01:06 -0800

    Remove broadcastSession placeholder

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -51,10 +51,6 @@ angular.module( 'moviematch.services', [] )
       } );
     },
 
-    broadcastSession: function( sessionName ) {
-
-    },
-
     fetchSessions: function() {
       return $http.get ( '/api/sessions' )
       .then( function( response ) {
