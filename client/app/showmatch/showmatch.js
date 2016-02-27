ea9cfb9a6bb840f91054f6c2777e76478b44b96b
Jake Shasteen <jakeshasteen@gmail.com>
2016-02-27 12:36:50 -0800

    Add client-side request for fetching a single movie

--- client/app/showmatch/showmatch.js
+++ client/app/showmatch/showmatch.js
@@ -1,6 +1,6 @@
 angular.module( 'moviematch.showmatch', [] )
 
-  .controller( 'ShowmatchController', function( $scope, Session, Auth ) {
+  .controller( 'ShowmatchController', function( $scope, FetchMovies, Session, Auth, $routeParams ) {
 
     Session.getSession()
     .then( function( session ) {
@@ -11,4 +11,12 @@ angular.module( 'moviematch.showmatch', [] )
     $scope.imgPath = 'http://image.tmdb.org/t/p/w500';
 
     $scope.currMovie = {};
+    var id = parseInt( $routeParams.id );
+
+    FetchMovies.getMovie( id )
+    .then( function( movie ) {
+      $scope.currMovie = movie;
+    });
+
+
   });
