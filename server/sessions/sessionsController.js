e6a0f92d63af5811671ccfdf769e1b4b44500332
Jake Shasteen <jakeshasteen@gmail.com>
2016-02-27 00:40:24 -0800

    Refactor to use shorthand req res

--- server/sessions/sessionsController.js
+++ server/sessions/sessionsController.js
@@ -6,29 +6,29 @@ module.exports = {
   getAllSessions: function( reqw, res, next ) {
     Session.findAll()
     .then( function( sessions ) {
-      response.send( sessions );
+      res.send( sessions );
     })
   },
 
   addSession: function( req, res, next ) {
-    var sessionName = request.body.sessionName;
+    var sessionName = req.body.sessionName;
 
     Session.create( {
       sessionName: sessionName
     } ).then( function() {
-      response.status = 201;
-      response.end();
+      res.status = 201;
+      res.end();
     } )
   },
 
   getSessionByName: function( req, res, next ) {
-    var sessionName = request.params.sessionName;
+    var sessionName = req.params.sessionName;
 
     Session.findOne( { where: { sessionName: sessionName } } )
     .then( function( session ) {
-      response.json( session );
+      res.json( session );
     }, function( err ) {
-      helpers.errorHandler( err, request, response, next );
+      helpers.errorHandler( err, req, res, next );
     });
   }
   
