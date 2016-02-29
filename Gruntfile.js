9f9a13d3e71aaa21adef2186a5636a31d0353e8b
Jake Shasteen <jakeshasteen@gmail.com>
2016-02-29 11:07:56 -0800

    Add missing {} in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,24 +19,26 @@ module.exports = function(grunt) {
           // require: 'coverage/blanket'
         },
         src: ['test/{server/,*.js}**/*.js']
+      }
+    },
+    coverage: {
+      options: {
+        reporter: 'html-cov',
+        // use the quiet flag to suppress the mocha console output
+        quiet: true,
+        // specify a destination file to capture the mocha
+        // output (the quiet option does not suppress this)
+        captureFile: 'coverage.html'
       },
-      coverage: {
-        options: {
-          reporter: 'html-cov',
-          // use the quiet flag to suppress the mocha console output
-          quiet: true,
-          // specify a destination file to capture the mocha
-          // output (the quiet option does not suppress this)
-          captureFile: 'coverage.html'
-        },
-        src: ['test/{server/,*.js}*.js']
+      src: ['test/{server/,*.js}*.js']
+    },
     // to run karma 'grunt karma' from terminal
     karma: {
       unit: {
         configFile: 'karma.conf.js'
       }
     }
-  };
+  });
 
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-karma');
