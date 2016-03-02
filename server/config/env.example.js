3f40f3c98745de82e26c8f5f2ffb2bd7065537b8
Alice Yu <yaliceme@gmail.com>
2016-03-02 11:57:29 -0800

    Revert facebook stuff

--- server/config/env.example.js
+++
@@ -1,9 +0,0 @@
-// When you require('env.js') because this isn’t wrapped
-// in a module.exports (on purpose), the code is literally
-// executed and thus the variables are added to your environment.
-//
-// Access by 'process.env.clientID'
-
-process.env['FACEBOOK_APP_ID'] = '';
-process.env['FACEBOOK_SECRET'] = '';
-process.env['FACEBOOK_URL'] = '';
