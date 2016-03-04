dbdde6740766581339114efa7ca327deafb4b592
Peter Lollo <peteralollo@gmail.com>
2016-03-03 16:36:05 -0800

    Adjust spacing

--- server/movies/moviesController.js
+++ server/movies/moviesController.js
@@ -24,14 +24,20 @@ module.exports = {
 
   getSearchResults: function (req, res, next) {
     var query = req.body.query;
-    var options = { method: 'GET',
-     url: 'http://api.themoviedb.org/3/search/movie',
-     qs:
-      { api_key: 'c062102bb9e2f7a7ab5d2a0f7fb7f2d0',
-        query: query },
-     headers:
-      { 'postman-token': '617cd5a3-78db-0ae0-0d80-cffed6da26e4',
-        'cache-control': 'no-cache' } };
+    var options = {
+      method: 'GET',
+      url: 'http://api.themoviedb.org/3/search/movie',
+      qs:
+        {
+          api_key: 'c062102bb9e2f7a7ab5d2a0f7fb7f2d0',
+        query: query
+      },
+      headers:
+        {
+          'postman-token': '617cd5a3-78db-0ae0-0d80-cffed6da26e4',
+          'cache-control': 'no-cache'
+        }
+      };
 
     request(options, function (error, response, body) {
      if (error) throw new Error(error);
