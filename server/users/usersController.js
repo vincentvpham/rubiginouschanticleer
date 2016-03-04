744487556155e2ec93b978d7f03409df77938b7d
Alice Yu <yaliceme@gmail.com>
2016-03-04 12:33:34 -0800

    Signup users using username only (no password), eliminate signin

--- server/users/usersController.js
+++ server/users/usersController.js
@@ -4,44 +4,35 @@ var jwt = require( 'jwt-simple' );
 module.exports = {
   getAllUsers: function() {},
 
-  signin: function (request, response) {
-  var username = request.body.username;
-  var password = request.body.password;
+  // signin: function (request, response) {
+  // var username = request.body.username;
+  // var password = request.body.password;
 
-  User.findOne({where: {username: username}})
-    .then(function (user) {
-      if (!user) {
-        response.redirect('/signin');
-      } else {
-          if (password === user.dataValues.password) {
-            var token = jwt.encode(user, 'secret');
-            response.json({token: token});
-          } else {
-            response.redirect('/signin');
-          }
-        }
-    });
-  },
+  // User.findOne({where: {username: username}})
+  //   .then(function (user) {
+  //     if (!user) {
+  //       response.redirect('/signin');
+  //     } else {
+  //         if (password === user.dataValues.password) {
+  //           var token = jwt.encode(user, 'secret');
+  //           response.json({token: token});
+  //         } else {
+  //           response.redirect('/signin');
+  //         }
+  //       }
+  //   });
+  // },
 
   signup: function(request, response) {
     var username = request.body.username;
-    var password = request.body.password;
 
-    User.findOne({where: {username: username}})
-      .then(function (user) {
-        if (user) {
-          response.send("username already exist, choose another username!");
-        } else {
-          User.create({
-            username: username,
-            password: password
-          })
-          .then(function (user) {
-            var token = jwt.encode(user, 'secret');
-            response.json({token: token});
-          });
-        }
-      });
+    User.create({
+      username: username
+    })
+    .then(function (user) {
+      var token = jwt.encode(user, 'secret');
+      response.json({token: token});
+    });
   },
 
   validate: function (request, response) {
